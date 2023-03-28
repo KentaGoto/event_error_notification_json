9bb5dd71d0a47fbcd655887cebce591505a75df2
KentaGoto <kenta-goto@to-in.co.jp>
2023-03-28 10:53:18 +0900

    minor revision

--- event_error_notification_json.bat
+++ event_error_notification_json.bat
@@ -3,12 +3,16 @@
 setlocal
 
 rem Exporting system event errors to json
+echo Exporting system event errors...
 wmic ntevent where "(logfile='system' and timegenerated >= '%DATE:/=%' and (EventType='1' or EventType='2'))" list /format:"%~dp0json.xsl" > "%~dp0%COMPUTERNAME%_%DATE:/=%_sys.json"
 
 rem Exporting application event errors to json
+echo Exporting application event errors...
 wmic ntevent where "(logfile='application' and timegenerated >= '%DATE:/=%' and (EventType='1' or EventType='2'))" list /format:"%~dp0json.xsl" > "%~dp0%COMPUTERNAME%_%DATE:/=%_app.json"
 
 echo Complete!
 echo Each log is written to %~dp0%
+
+pause > nul
 
 endlocal
